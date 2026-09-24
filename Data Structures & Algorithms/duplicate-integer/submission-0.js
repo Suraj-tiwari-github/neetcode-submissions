5f2f7c2241d38c56fb6dc61a157a357fded42128
Suraj Tiwari <surajtiwari9sep@gmail.com>
2026-09-24 14:22:23 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const numsSet=new Set(nums);
+
+        if(nums.length>numsSet.size){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
