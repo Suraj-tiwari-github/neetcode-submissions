52d8404387103852b02dfe5b8304cffe967265e8
Suraj Tiwari <surajtiwari9sep@gmail.com>
2026-09-24 13:45:53 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let maxProfit=0;
+        let buy=0;
+        let sell=1;
+        while(sell<prices.length){
+            if(prices[sell]-prices[buy]<=0){
+                buy=sell;
+                sell++            
+            }
+            else{
+                if(prices[sell]-prices[buy]>maxProfit){
+                    maxProfit=prices[sell]-prices[buy]
+                }
+                sell++;
+            }
+        }
+
+        return maxProfit;
+    }
+}
